a3483daa3a185950b70081f8293fc9b9dc51c6b4
csqueiroz <csqueiroz@MacBook-Pro-de-Camila.local>
2022-04-05 18:49:51 +0100

    Initial commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,13 @@
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>ig.news - Home</title>
+      </Head>
+
+      <h1>Hi there</h1>
+    </>
+  );
+}
